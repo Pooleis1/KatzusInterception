81fc59a34b95d35559d206c2105515e36aaa4b22
Pia <P!a2GitHub>
2022-04-24 19:20:05 +0200

    Gamemechanik begonnen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    //Alle Public Attribute werden automatisch Seralizied
+    public float movementSpeed;
+    //Mit dem SerializeField kann man auch private Attribute Seralizied
+    [SerializeField]
+    private float JumpForce;
+
+    public GameObject map;
+    private bool keyIsDown;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Starten");
+        keyIsDown = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("m")&&keyIsDown==false)
+        {
+            Instantiate(map,new Vector3(0,0,0), Quaternion.identity);
+
+            keyIsDown = true;
+        }
+
+        if (Input.GetKeyDown("m") && keyIsDown == true)
+        {
+            Destroy(gameObject);
+            keyIsDown = false;
+        }
     }
 }
