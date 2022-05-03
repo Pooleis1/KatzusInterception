c75e0d8364d7a884dbac1f7b5975185ba95b145b
Pia <parnold@student.tgm.ac.at>
2022-05-03 23:30:12 +0200

    verbesserungen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,22 +7,20 @@ public class PlayerController : MonoBehaviour
 {
     
     //Alle Public Attribute werden automatisch Seralizied
-    public float movementSpeed = 7f;
+    public float moveSpeed = 3f;
     //Mit dem SerializeField kann man auch private Attribute Seralizied
     [SerializeField] private float jumpForce = 4f;
     
-    private Rigidbody2D transform;
+    private Rigidbody2D rb;
     
 
     public GameObject map;
     private bool keyIsDown;
 
+    private bool canDoubleJump = true;
+    private CapsuleCollider2D collider;
 
-
-    private bool isGrounded = true;
-    
-    
-    
+    private LayerMask platformLayer = 8;
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +28,10 @@ public class PlayerController : MonoBehaviour
         Debug.Log("Starten");
         keyIsDown = false;
 
-        transform = GetComponent<Rigidbody2D>();
+        //Wir nehmen ein Component von unserm Game und dieses Gameobject wird zu einem Rigidbody formatiert
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+        collider = gameObject.GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
@@ -37,29 +39,32 @@ public class PlayerController : MonoBehaviour
     {
         //GetAxis gibt einen wert von -1,0,1 zurück bei drücken von den a,nichts,d
         float h = Input.GetAxis("Horizontal");
-        
-        
+ 
         //bewegt den Spieler um die X Achse vorwärts. -1*movespeed,0,1*movespeed
-        transform.velocity = new Vector2(h * movementSpeed, transform.velocity.y);
+        rb.velocity = new Vector2(h * moveSpeed, rb.velocity.y);
 
-        if (isGrounded)
+
+        /*if (IsGrounded())
+        {
+            canDoubleJump = true;
+        }*/
+
+        if (IsGrounded())
         {
             float v = Input.GetAxis("Vertical");
-            transform.velocity = new Vector2(transform.velocity.x, v*jumpForce);
-            // isGrounded = false;
-        }
-        
-        /*RaycastHit2D raycastHit = Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-        return raycastHit.collider != null;
-
-        if ()
+            rb.velocity = new Vector2(rb.velocity.x, v*jumpForce);
+        } /*else
         {
-            isGrounded = true;
+            if (canDoubleJump)
+            {
+                float v = Input.GetAxis("Vertical");
+                rb.velocity = new Vector2(rb.velocity.x, v*jumpForce);
+                canDoubleJump = false;
+            }
         }*/
-        
 
-        
-        
+
+
         /*if (Input.GetKeyDown("m")&&keyIsDown==false)
         {
             Instantiate(map,new Vector3(0,0,0), Quaternion.identity);
@@ -75,5 +80,12 @@ public class PlayerController : MonoBehaviour
         
 
     }
-    
+
+    private bool IsGrounded()
+    {
+
+        RaycastHit2D raycastHit = Physics2D.CapsuleCast(collider.bounds.center, collider.bounds.size, 0,0f, Vector2.down, 0.1f,platformLayer);
+        Debug.Log(raycastHit.collider);
+        return raycastHit.collider != null;
+    }
 }
