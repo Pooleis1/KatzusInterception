b5f2581c0c43cde6db5c20f044f3480544d5ec8e
Pia <P!a2GitHub>
2022-05-06 12:01:47 +0200

    Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,14 +11,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce = 4f;
     
     private Rigidbody2D transform;
+
+
+    private CapsuleCollider2D collider;
+    [SerializeField] private LayerMask groundLayer;
     
 
     public GameObject map;
     private bool keyIsDown;
-
-
-
-    private bool isGrounded = true;
+    
     
     
     
@@ -30,6 +31,7 @@ public class PlayerController : MonoBehaviour
         keyIsDown = false;
 
         transform = GetComponent<Rigidbody2D>();
+        collider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
@@ -38,21 +40,19 @@ public class PlayerController : MonoBehaviour
         //GetAxis gibt einen wert von -1,0,1 zurück bei drücken von den a,nichts,d
         float h = Input.GetAxis("Horizontal");
         
-        
         //bewegt den Spieler um die X Achse vorwärts. -1*movespeed,0,1*movespeed
         transform.velocity = new Vector2(h * movementSpeed, transform.velocity.y);
 
-        if (isGrounded)
+
+        if (isGrounded())
         {
             float v = Input.GetAxis("Vertical");
             transform.velocity = new Vector2(transform.velocity.x, v*jumpForce);
             // isGrounded = false;
         }
-        
-        /*RaycastHit2D raycastHit = Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-        return raycastHit.collider != null;
 
-        if ()
+
+        /**if ()
         {
             isGrounded = true;
         }*/
@@ -75,5 +75,18 @@ public class PlayerController : MonoBehaviour
         
 
     }
-    
+
+    /**
+     * Schaut ob er in der Luft ist oder nicht
+     * @return Ob er sich Am Boden befindet oder nicht
+     */
+    private bool IsGrounded()
+    {
+        //Sagt das er nur !null ist wenn er den spezifischen groundLayer unten berührt
+        RaycastHit2D raycastHit = Physics2D.CapsuleCast(collider.bounds.center, collider.bounds.size,0, 0, Vector2.down, 0.1f, groundLayer);
+        print(raycastHit.collider);
+        //gibt null zurück der Collider nicht den ground berührt
+        return raycastHit.collider != null;
+    }
+
 }
